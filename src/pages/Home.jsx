ff1ee5dd4e2dd1b116720399f4c05ee6466c8040
Med-lemineHmd <leminlemin00@gmail.com>
2023-01-19 15:26:00 +0100

    useEffect Hooks

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Footer, Header } from "../components";
 
 const Home = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        setUsers(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  // useEffect(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setUsers(data);
+  //       console.log(data);
+  //     })
+  //     .catch((err) => console.log(err));
+  // }, []);
+
   return (
     <>
       <Header title="This is a Header" />
-      <main></main>
+      <main>
+        {users.map((user) => (
+          <div key={user.id}>
+            <p>{user.name}</p>
+          </div>
+        ))}
+      </main>
       <Footer title="This is a Footer" />
     </>
   );
