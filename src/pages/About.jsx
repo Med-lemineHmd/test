90c8ef0bf7591c33aff00998aad4a0ee39867849
Med-lemineHmd <leminlemin00@gmail.com>
2023-01-20 00:02:52 +0100

    useRef Hooks

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from "react";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  const [posts, setPosts] = useState("");
 
-export default About
+  const refInput = useRef();
+
+  useEffect(() => {
+    refInput.current.focus();
+    refInput.current.setAttribute("id", "type");
+    console.log(refInput);
+  });
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <div>
+        <label htmlFor="">Your Text: </label>
+        <input
+          type="text"
+          name=""
+          ref={refInput}
+          onChange={(e) => setPosts(e.target.value)}
+        />
+      </div>
+      <div>
+        <h2>{posts}</h2>
+      </div>
+    </div>
+  );
+};
+
+export default About;
